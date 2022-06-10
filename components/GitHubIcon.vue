876704140ee1c54eda37ddfe66346fd0e23d5292
krabben16 <57534374+krabben16@users.noreply.github.com>
2022-06-10 18:56:41 +0900

    add マークダウン、vueファイルを作成

---
+++ components/GitHubIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps({
+  id: {
+    default: '',
+  },
+})
+</script>
+
+<template>
+  <a :href="`https://github.com/${id}`" target="_blank" alt="GitHub"
+    class="text-xl icon-btn opacity-50 !border-none !hover:text-white">
+    <carbon-logo-github />
+  </a>
+</template>
---
+++ components/Profile.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const {
+  VITE_COMPANY_NAME,
+  VITE_SECTION_NAME,
+  VITE_PERSONAL_NAME,
+} = import.meta.env
+</script>
+
+<template>
+  <div>
+    {{ VITE_COMPANY_NAME }} {{ VITE_SECTION_NAME }} {{ VITE_PERSONAL_NAME }}
+  </div>
+</template>
